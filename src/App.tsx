cca422986722a155725a37c047fa993cd1378726
Bruno Bulnes <bulnes.bruno@gmail.com>
2022-08-14 22:19:56 -0300

    feat: installing styled-componets lib

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 60px;
+  color: purple;
+`
+
 export function App() {
   return (
     <div className="App">
-      <h1>Hello World</h1>
+      <Title>Hello World</Title>
     </div>
   );
 }
+
